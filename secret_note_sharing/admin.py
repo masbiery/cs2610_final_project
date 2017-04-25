c1bdc5a8faed2919f34ac3232f24d5a119f1ba2b
Mbiery <masbiery@gmail.com>
2017-04-25 03:35:08 +0000

    added a key to entries in the database that will be used to access the message, keys are now viewable in the admin page.

--- secret_note_sharing/admin.py
+++ secret_note_sharing/admin.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 
 from .models import Message
 
-admin.site.register(Message)
+class Admin(admin.ModelAdmin):
+    list_display = ('message_text','id',)
+    
+admin.site.register(Message,Admin)
--- secret_note_sharing/models.py
+++ secret_note_sharing/models.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
+import uuid
 
 from django.db import models
 
 class Message(models.Model):
-    message_text = models.CharField(max_length=1000)
+    message_text = models.CharField(max_length=50)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     def __str__(self):
         return self.message_text
 
