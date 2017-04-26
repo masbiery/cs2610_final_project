384c31503b1cf9f6f78a3ad007e5cf847e359449
Mbiery <masbiery@gmail.com>
2017-04-26 02:54:54 +0000

    pk is now showing on the submitted page. message can be retrieved using the pk.

--- secret_note_sharing/urls.py
+++ secret_note_sharing/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
-    url(r'^submitted/', views.message_submitted, name='message_submitted'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<message_id>.*)/$', views.message_retrieve, name='message_retrieve'),
 ]
--- secret_note_sharing/views.py
+++ secret_note_sharing/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.core.urlresolvers import reverse
 from secret_note_sharing.models import Message
 from secret_note_sharing.forms import SubmitForm
@@ -11,9 +11,20 @@ def index(request):
             message_text = request.POST.get('message_text', '')
             message_obj = Message(message_text = message_text)
             message_obj.save()
-        return render(request, 'secret_note_sharing/message_submitted.html')
+            message_id = message_obj.pk
+        return HttpResponse(message_submitted(request, message_id))
     return render(request,'secret_note_sharing/index.html')
 
-def message_submitted(request):
-    return render(request, 'secret_note_sharing/message_submitted.html')
+def message_submitted(request, message_id):
+    message = Message.objects.get(pk=message_id)
+    return render(request, 'secret_note_sharing/message_submitted.html', {'message': message})
+    
+def message_retrieve(request, message_id):
+    try:
+        message = Message.objects.get(pk=message_id)
+    except Message.DoesNotExist:
+        raise Http404("Message does not exist")
+    return render(request, 'secret_note_sharing/message_retrieve.html', {'message': message})
+    
+
     
