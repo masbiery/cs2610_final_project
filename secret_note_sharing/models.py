e96a09b9ccb56e7f0cbcf1be036c3dd35e43d7cf
Mbiery <masbiery@gmail.com>
2017-04-29 20:03:25 +0000

    added javascript. Form message can now be changed from the encrypt function when the form is submitted. Still working on making cryptojs work properly.

--- secret_note_sharing/models.py
+++ secret_note_sharing/models.py
@@ -4,7 +4,7 @@ import uuid
 from django.db import models
 
 class Message(models.Model):
-    message_text = models.CharField(max_length=50)
+    message_text = models.CharField(max_length=200)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     def __str__(self):
         return self.message_text
